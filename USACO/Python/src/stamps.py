8d86ababcdf3c04e2824f46221bc377297c5aa6a
Roy <royone@hotmail.com>
2020-12-12 22:48:33 -0600

    Term end update for USACO

--- USACO/Python/src/stamps.py
+++ USACO/Python/src/stamps.py
@@ -7,13 +7,18 @@ fin=open(fileName + '.in')
 k, n = map(int, fin.readline().split())
 stamps = [int(i) for line in fin.readlines() for i in line.split()]
 
-p = [0] * (k*max(stamps)+2)
+p = [201] * (k*max(stamps)+2)
+p[0] = 0
 
-for i in range(1, len(p)):
-    p[i] = min(p[i-v] for v in stamps if i >= v) + 1
-    if p[i] > k:
+for v in stamps:
+    for i in range(v, len(p)):
+        if p[i-v] < k:
+            p[i]=min(p[i], p[i-v]+1)
+
+for i in range(len(p)):
+    if p[i]>k:
         break
-    
+
 fout = open(fileName + '.out', 'w')
 fout.write('%s\n' % (i-1))
 fout.close()
